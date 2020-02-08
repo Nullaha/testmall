e3ed25ef93d4a13f5d234a406aa7d4ad314ca406
Nullaha <swdyihehe@qq.com>
2020-02-08 20:33:06 +0800

    '2020-2-8'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,22 +1,47 @@
 <!--  -->
 <template>
   <div id="home">
-    <nav-bar><div slot="center">购物街</div></nav-bar>
-    <h2>首页</h2>
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <home-recommend-view :recommends="recommends"></home-recommend-view>
   </div>
 </template>
 
 <script>
   import NavBar from 'components/common/navbar/NavBar.vue'
+  import HomeSwiper from './childComps/HomeSwiper'
+  import HomeRecommendView from './childComps/HomeRecommendView'
+
+  import { getHomeMultidata } from "network/home";
 
 export default {
   name: "Home",
   components:{
     NavBar,
+    HomeSwiper,
+    HomeRecommendView,
+
+  },
+  data(){
+    return {
+      banners:[],
+      recommends:[],
+    }
+  },
+  created(){
+    //1请求多个数据
+    getHomeMultidata().then(res=>{
+      console.log(res);
+      this.banners=res.data.banner.list;
+      this.recommends=res.data.recommend.list;
+    })
   }
 };
 </script>
 
 <style scoped>
-
+  .home-nav{
+    background-color: var(--color-tint);
+    color: #fff;
+  }
 </style>
