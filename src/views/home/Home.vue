ac59496f63a63dfab0e7b18f29b511bce7a6846a
Nullaha <swdyihehe@qq.com>
2020-02-09 19:28:34 +0800

    '2020-2-9'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,28 +4,143 @@
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
     <home-swiper :banners="banners"></home-swiper>
     <home-recommend-view :recommends="recommends"></home-recommend-view>
+    <home-feature-view></home-feature-view>
+    <tab-control class="tab-control" :titles="['流行','精选','新款']"></tab-control>
+
+    <ul>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+    </ul>
+
   </div>
 </template>
 
 <script>
-  import NavBar from 'components/common/navbar/NavBar.vue'
+  // 子组件
   import HomeSwiper from './childComps/HomeSwiper'
   import HomeRecommendView from './childComps/HomeRecommendView'
-
-  import { getHomeMultidata } from "network/home";
+  import HomeFeatureView from './childComps/HomeFeatureView'
+  // 公共组件
+  import NavBar from 'components/common/navbar/NavBar.vue'
+  import TabControl from 'components/content/tabControl/TabControl.vue'
+  // 其他
+  import { getHomeMultidata,getHomeGoods } from "network/home";
 
 export default {
   name: "Home",
   components:{
-    NavBar,
     HomeSwiper,
     HomeRecommendView,
+    HomeFeatureView,
+    NavBar,
+    TabControl,
 
   },
   data(){
     return {
       banners:[],
       recommends:[],
+      goods:{
+        'pop':{page:0,list:[]},
+        'new':{page:0,list:[]},
+        'sell':{page:0,list:[]}
+      }
     }
   },
   created(){
@@ -35,13 +150,32 @@ export default {
       this.banners=res.data.banner.list;
       this.recommends=res.data.recommend.list;
     })
+    // 2请求商品数据
+    getHomeGoods().then(res=>{
+      console.log(res)
+    })
   }
 };
 </script>
 
 <style scoped>
+  #home{
+    padding-top: 44px;
+  }
+
   .home-nav{
     background-color: var(--color-tint);
     color: #fff;
+
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 999;
+  }
+
+  .tab-control{
+    position: sticky;
+    top: 44px;
   }
 </style>
